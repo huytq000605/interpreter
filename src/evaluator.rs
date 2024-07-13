5a9b29697491e61c02a7aac79a8891796ef659e8
huy.tran <huy.tran@bytedance.com>
2024-07-14 03:17:07 +0800

    evaluator WIP

--- src/evaluator.rs
+++ src/evaluator.rs
@@ -2,17 +2,52 @@ use crate::parser::Program;
 use crate::statement::{ExpressionStatement, ExpressionStatement::*, Statement::*};
 use crate::token::Token;
 use std::collections::HashMap;
-use std::fmt::format;
+use std::env::var;
+use std::ops;
 
 pub struct Evaluator {
     program: Program,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Object {
     Number(f64),
     String(String),
     Null,
+}
+
+impl ops::Add for Object {
+    type Output = Result<Object, String>;
+
+    fn add(self, rhs: Self) -> Result<Object, String> {
+        return match self {
+            Self::Number(num1) => match rhs {
+                Self::Number(num2) => Ok(Self::Number(num1 + num2)),
+                Self::String(s2) => Ok(Self::String(format!("{}{}", num1, s2))),
+                Self::Null => return Err(format!("Invalid value rhs = NULL"))
+            },
+            Self::String(s1) => match rhs {
+                Self::Number(num2) => Ok(Self::String(format!("{}{}", s1, num2))),
+                Self::String(s2) => Ok(Self::String(s1 + &s2)),
+                Self::Null => return Err(format!("Invalid value rhs = NULL"))
+            },
+            Self::Null => return Err(format!("Invalud value lhs = NULL"))
+        }
+    }
+}
+
+impl ops::Sub for Object {
+    type Output = Result<Object, String>;
+
+    fn sub(self, rhs: Self) -> Result<Object, String> {
+        return match self {
+            Self::Number(num1) => match rhs {
+                Self::Number(num2) => Ok(Self::Number(num1 - num2)),
+                _ => return Err(format!("Invalid value rhs = {:?}", self))
+            },
+            _ => return Err(format!("Invalud value lhs = {:?}", self))
+        }
+    }
 }
 
 struct Environment {
@@ -24,10 +59,24 @@ impl Evaluator {
         return Evaluator { program };
     }
 
-    fn eval(self, environment: Environment) {
+    fn eval(self, environment: Environment){
         for statement in self.program.statements.into_iter() {
             match statement {
                 Let(variable_name, value) => {
+                    match value {
+                        None => environment.variables.insert(variable_name, Object::Null),
+                        Some(expr) => {
+                            let v = self.eval_expression(&environment, &expr);
+                            match v {
+                                Err(e) => panic!("{}", e),
+                                Ok(v) => {
+                                    environment.variables.insert(variable_name, v);
+                                }
+                            }
+                        }
+                    }
+                    let mut v = Object::Null;
+                    if value
                     // environment.variables.insert(variable_name, Object)
                 }
                 Return(return_value) => {}
@@ -36,10 +85,10 @@ impl Evaluator {
         }
     }
 
-    fn eval_expression(self, expr: &ExpressionStatement) -> Result<Object, String> {
+    fn eval_expression(&self, environment: &Environment, expr: &ExpressionStatement) -> Result<Object, String> {
         match expr {
             Prefix { operator, right } => {
-                let mut v = self.eval_expression(right)?;
+                let mut v = self.eval_expression(environment, right)?;
                 v = match operator {
                     Token::Bang => match v {
                         Object::Number(v) => {
@@ -78,7 +127,15 @@ impl Evaluator {
                 left,
                 operator,
                 right,
-            } => return Err("Unimplemented".to_string()),
+            } => {
+                let lhs = self.eval_expression(environment, left)?;
+                let rhs = self.eval_expression(environment, right)?;
+                match *operator {
+                    Token::Plus => lhs + rhs,
+                    Token::Minus => lhs - rhs,
+                    _ => return Err(format!("Invalid infix operator {:?}", operator))
+                }
+            },
             If {
                 condition,
                 outcome,
@@ -86,8 +143,13 @@ impl Evaluator {
             } => return Err("Unimplemented".to_string()),
             Fn { args, body } => Err("Unimplemented".to_string()),
             Call { caller, args } => Err("Unimplemented".to_string()),
-            Group(expr) => self.eval_expression(expr),
-            Identifier(s) => Ok(Object::String(s.to_string())),
+            Group(expr) => self.eval_expression(environment, expr),
+            Identifier(s) => {
+                match environment.variables.get(s) {
+                    Some(v) => Ok(v.to_owned()),
+                    None => Err(format!("Undefined variable {}", s)),
+                }
+            },
             Num(num) => Ok(Object::Number(*num)),
             Bool(b) => {
                 if *b {
